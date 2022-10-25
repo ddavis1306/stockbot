d407e62a51a925d68923b457d9f4a2c5a6257928
Darryl Davis <ddavis1306@users.noreply.github.com>
2022-10-24 19:47:43 -0700

    still trying to understand json

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-#/usr/bin/env/ python3
-import config
-
-from twython import Twython, TwythonError
-
-# create a Twython object by passing the necessary secret passwords
-twitter = Twython(config.api_key, config.api_secret, config.access_token, config.token_secret)
-
-
+#!/usr/bin/env/ python3
+from config import api_key
 
 from bestbuy.apis import BestBuy
 bb = BestBuy(api_key)
-    
-bb.ProductAPI.search_by_sku(sku=9776457)
+#a_prod = bb.ProductAPI.search(query="sku=6514052", format="json")
+a_prod = bb.ProductAPI.search_by_sku(sku=6514052)
 bb.ProductAPI.search(searchTerm="hard drive", onSale="true")
 bb.StoreAPI.search_by_city(city="Atlanta")
 bb.CategoryAPI.search_by_category_id(category_id="abcat0011001")
+print(bb.BestBuy.search_by_sku(sku=6514052))
