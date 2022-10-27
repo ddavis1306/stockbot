9997bfb7eaa6414aae35ec66cffe5bb4a15edad2
Darryl Davis <ddavis1306@users.noreply.github.com>
2022-10-26 19:18:46 -0700

    targeted sku's added add to cart and online avail

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/ python3
+#!/usr/bin/env python3
 from config import api_key
 import os
 import json
@@ -6,11 +6,13 @@ import requests
 #from bestbuy.apis import BestBuy
 bb = api_key
 #print(bb)
-response_API = requests.get('https://api.bestbuy.com/v1/products((search=Sony)&freeShipping=true&(categoryPath.id=abcat0204000))?apiKey=uKxiL0ChCxCWmxGLSLpIBHxS&show=addToCartUrl&facet=inStoreAvailability,10&format=json')
+sku = input("please enter the sku you want to monitor \n")
+response_API = requests.get("https://api.bestbuy.com/v1/products/" + sku + ".json?apiKey=uKxiL0ChCxCWmxGLSLpIBHxS")
 print(response_API.status_code)
-data = response_API.text
-parse_json = json.loads(data)
-headphones = parse_json['products'][0]
-print(headphones)
-
-
+data = response_API.json()
+print(data)
+print(data["addToCartUrl"])
+print(data["onlineAvailability"])
+# parse_json = json.loads(data)
+# headphones = parse_json['products']['']
+# print(headphones)
