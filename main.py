257e792e65c389244358fd23ca0b43494f2f55c5
Darryl Davis <ddavis1306@users.noreply.github.com>
2022-10-26 21:51:17 -0700

    add to cart funtionality added. passes ATC AND product name

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from config import api_key
 import os
 import json
 import requests
-import sendMsg
+from twilio.rest import Client
+from config import account_sid
+from config import auth_token
+# import sendMsg
 def main():
 #from bestbuy.apis import BestBuy
     bb = api_key
@@ -13,16 +16,23 @@ def main():
     sku = input("please enter the sku you want to monitor \n")
     response_API = requests.get("https://api.bestbuy.com/v1/products/" + sku + ".json?apiKey=uKxiL0ChCxCWmxGLSLpIBHxS")
     print(response_API.status_code)
-    DATA = response_API.json()
+    data = response_API.json()
     #print(data)
     #^shows the entine json
-    if DATA["onlineAvailability"] == True:
+    if data["onlineAvailability"] == True:
         print("its in stock")
-        sendMsg.instock(DATA)
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+            to="+12064783243", 
+            from_="+16802213523",
+        body="add to cart!" + data["addToCartUrl"] + "\n" + data["name"] + "is in stock")
+
+        print(message.sid)
     else:
         print("OOS try again later")
-    print(DATA["addToCartUrl"])
-    print(DATA["onlineAvailability"])
+    print(data["addToCartUrl"])
+    print(data["onlineAvailability"])
     # parse_json = json.loads(data)
     # headphones = parse_json['products']['']
     # print(headphones)
