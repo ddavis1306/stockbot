9843582ad96c19efd85469b754fc4e18af93960d
ddavis1306 <81946494+ddavis1306@users.noreply.github.com>
2022-11-01 14:36:48 -0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env/ python3
-"""MUST BE IN .GITIGNORE"""
-# .gitignore should include reference to config.py
-api_key = "uKxiL0ChCxCWmxGLSLpIBHxS"
-account_sid = 'AC4c65499dcd25742554db001888640acb'
-auth_token = '0feaf7e4172ff37e9413891cc883e933'
-gmail_pwd = 'rtbppijrpaseorzg'
-sender = 'darryldev2022@gmail.com'
